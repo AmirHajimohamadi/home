cb38bee3a0103685b10ec3f648853206655672e1
IT-Pioneers <amir.hajimohamadi@outlook.com>
2021-07-09 10:57:18 +0430

    Updates

---
+++ precache-manifest.ba7ced323d83af03217aad5deac8a840.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0195f7d24bf46d77377ffa6f155a0b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaf66aec143f41cab7a4",
+    "url": "/static/css/main.9d1d1a0e.chunk.css"
+  },
+  {
+    "revision": "2df25981f784d8b2fb26",
+    "url": "/static/js/2.58e5bfff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.58e5bfff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaf66aec143f41cab7a4",
+    "url": "/static/js/main.f1c4188b.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "cf1718867c42b5264c900e15f0245307",
+    "url": "/static/media/amirhajimohamadi.cf171886.png"
+  },
+  {
+    "revision": "ebfeb1cae17c06e48964c832e5549ab4",
+    "url": "/static/media/resume.ebfeb1ca.pdf"
+  }
+]);
